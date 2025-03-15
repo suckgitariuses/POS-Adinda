6e80226c5c732c020b3dc54ca7981d2da487debb
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-15 15:52:03 +0700

    praktikum 2.5.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,29 +10,21 @@ class UserController extends Controller
 {
     public function index() {
         $user = UserModel::create([
-            'username' => 'manager55',
-            'nama' => 'Manager55',
+            'username' => 'manager11',
+            'nama' => 'Manager11',
             'password' => Hash::make('12345'),
             'level_id' => 2,
         ]);
 
-        $user->username = 'manager56';
-
-        $user->isDirty(); // true
-        $user->isDirty('username'); // true
-        $user->isDirty('nama'); // true
-        $user->isDirty(['nama', 'username']); // true
-
-        $user->isClean(); // false
-        $user->isClean('username'); // false
-        $user->isClean('nama'); // false
-        $user->isClean(['nama', 'username']); // false
+        $user->username = 'manager12';
 
         $user->save();
 
-        $user->isDirty(); // false
-        $user->isClean(); // true
-        dd($user->isDirty());
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged(['username', 'level_id']); // true
+        $user->wasChanged('nama'); // false
+        dd($user->wasChanged(['nama', 'username'])); // true
     }
 }
 
