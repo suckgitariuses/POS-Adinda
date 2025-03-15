d13bae5119dfe392d0ed929d91e79cdc8e6d78a8
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-15 14:37:34 +0700

    praktikum 2.2.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::findOrFail(1);
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
 
 
