71bd66c49ddd748f4ad550938d88d6e10de7547c
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-15 14:21:38 +0700

    praktikum 2.1.8

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,9 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstWhere('level_id',1);
+        $user = UserModel::findOr(1, ['username', 'nama'], function (){
+            abort(404);
+        });
         return view('user', ['data' => $user]);
         // // tambah data user dengan Eloquent model 
         // $data = [
