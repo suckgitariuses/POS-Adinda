a53fa309351eb0722dc9d392a755a34af1ab0f7d
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-15 15:39:35 +0700

    praktikum 2.5.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,44 +8,31 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
-    {
-        $user = UserModel::firstOrNew(
-            [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-            ],
-        );
+    public function index() {
+        $user = UserModel::create([
+            'username' => 'manager55',
+            'nama' => 'Manager55',
+            'password' => Hash::make('12345'),
+            'level_id' => 2,
+        ]);
+
+        $user->username = 'manager56';
+
+        $user->isDirty(); // true
+        $user->isDirty('username'); // true
+        $user->isDirty('nama'); // true
+        $user->isDirty(['nama', 'username']); // true
+
+        $user->isClean(); // false
+        $user->isClean('username'); // false
+        $user->isClean('nama'); // false
+        $user->isClean(['nama', 'username']); // false
+
         $user->save();
 
-        return view('user', ['data' => $user]);
-
-        // // tambah data user dengan Eloquent model 
-        // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => ('Pelanggan'),
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-        // UserModel::insert($data);
-
-        // $data = [
-        //     'nama' => 'Pelanggan Pertama',
-        // ];
-        // UserModel::where('username', 'customer-1')->update($data);
-
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_tiga',
-        //     'nama' => 'Manager 3',
-        //     'password' => Hash::make('12345'),
-        // ];
-        // UserModel::create($data);
-
-        // // mengakses model usermodel
-        // $user = UserModel::all();
-        // return view('user', ['data' => $user]);
+        $user->isDirty(); // false
+        $user->isClean(); // true
+        dd($user->isDirty());
     }
 }
+
