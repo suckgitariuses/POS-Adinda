cedfa19278b2599782c59d412f8e483a93f75295
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-15 13:53:59 +0700

    praktikum 1 no 5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,8 +26,8 @@ class UserController extends Controller
 
         $data = [
             'level_id' => 2,
-            'username' => 'manager_dua',
-            'nama' => 'Manager 2',
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
             'password' => Hash::make('12345'),
         ];
         UserModel::create($data);
