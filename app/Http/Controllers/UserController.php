dd46b68220201b41b53e15e3a53dbdda30ba939c
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-15 16:02:56 +0700

    praktikum 2.6.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,22 +9,8 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
-        $user = UserModel::create([
-            'username' => 'manager11',
-            'nama' => 'Manager11',
-            'password' => Hash::make('12345'),
-            'level_id' => 2,
-        ]);
-
-        $user->username = 'manager12';
-
-        $user->save();
-
-        $user->wasChanged(); // true
-        $user->wasChanged('username'); // true
-        $user->wasChanged(['username', 'level_id']); // true
-        $user->wasChanged('nama'); // false
-        dd($user->wasChanged(['nama', 'username'])); // true
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
     }
 }
 
