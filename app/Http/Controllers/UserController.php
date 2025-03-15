35d608c58aecc80a37aee33f2ec031b9cc757101
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-15 14:24:53 +0700

    praktikum 2.1.10

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,7 +10,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::findOr(1, ['username', 'nama'], function (){
+        $user = UserModel::findOr(20, ['username', 'nama'], function (){
             abort(404);
         });
         return view('user', ['data' => $user]);
