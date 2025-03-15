1bb70699e891d244cf80edf7e8aa75af90145ec7
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-15 14:51:04 +0700

    praktikum 2.3.3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,7 +11,6 @@ class UserController extends Controller
     public function index()
     {
         $user = UserModel::where('level_id', 2)->count();
-        dd($user);
         return view('user', ['data' => $user]);
 
 
