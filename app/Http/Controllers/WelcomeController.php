ec8fd34a72055837b159c4a50e808652a1f18d5a
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-20 14:53:44 +0700

    Praktikum 1 2 (ulang)

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class WelcomeController extends Controller
-{
-    public function index()
-    {
+class WelcomeController extends Controller {
+    public function index() {
         $breadcrumb = (object) [
             'title' => 'Selamat Datang',
             'list'  => ['Home', 'Welcome']
