1759cb715ec5a91d877f74b8cfb0d4a79c8dc3f4
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-22 09:31:38 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -6,30 +6,19 @@ use App\Http\Controllers\LevelController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// // Route::get('/level', [LevelController::class, 'index']);
-
-// // Route::get('/kategori', [KategoriController::class, 'index']);
-
-// Route::get('/user', [UserController::class, 'index']);
-
-// Route::get('/user/tambah', [UserController::class, 'tambah']);
-
-// Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-
-// Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-
-// Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-
-// Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-
-// Route::get('/', [WelcomeController::class, 'index']);
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/', [WelcomeController::class, 'index']);
+
+Route::group(['prefix' => 'user'], function () {
+    Route::get('/', [UserController::class, 'index']); // Menampilkan daftar user
+    Route::post('/list', [UserController::class, 'list']); // Menampilkan list user
+    Route::get('/create', [UserController::class, 'create']); // Menampilkan form create user
+    Route::post('/', [UserController::class, 'store']); // Menyimpan user baru
+    Route::get('/{id}', [UserController::class, 'show']); // Menampilkan user tertentu
+    Route::get('/{id}/edit', [UserController::class, 'edit']); // Menampilkan form edit user
+    Route::put('/{id}', [UserController::class, 'update']); // Mengupdate user
+    Route::delete('/{id}', [UserController::class, 'destroy']); // Menghapus user
+});
