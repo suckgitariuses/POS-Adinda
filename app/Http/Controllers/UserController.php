394b1f1986b643d9dcdc0045cf2862c6ef77d636
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-03-23 10:55:47 +0700

    JOBSHEET6 : Praktikum 1 & 2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -38,12 +38,16 @@ class UserController extends Controller
         return DataTables::of($users)
             ->addIndexColumn()
             ->addColumn('aksi', function ($user) {
-                return '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a> '
-                    . '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> '
-                    . '<form method="POST" action="' . url('/user/' . $user->user_id) . '" style="display:inline-block;">
-                    ' . csrf_field() . method_field('DELETE') . '
-                    <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\"Apakah Anda yakin menghapus data ini?\");">Hapus</button>
-                    </form>';
+                // return '<a href="' . url('/user/' . $user->user_id) . '" class="btn btn-info btn-sm">Detail</a> '
+                //     . '<a href="' . url('/user/' . $user->user_id . '/edit') . '" class="btn btn-warning btn-sm">Edit</a> '
+                //     . '<form method="POST" action="' . url('/user/' . $user->user_id) . '" style="display:inline-block;">
+                //     ' . csrf_field() . method_field('DELETE') . '
+                //     <button type="submit" class="btn btn-danger btn-sm" onclick="return confirm(\"Apakah Anda yakin menghapus data ini?\");">Hapus</button>
+                //     </form>';
+                $btn = '<button onclick="modalAction(\''.url('/user/' . $user->user_id . '/show_ajax').'\')" class="btn btn-info btn-sm">Detail</button> ';
+                $btn .= '<button onclick="modalAction(\''.url('/user/' . $user->user_id . '/edit_ajax').'\')" class="btn btn-warning btn-sm">Edit</button> ';
+                $btn .= '<button onclick="modalAction(\''.url('/user/' . $user->user_id . '/delete_ajax').'\')" class="btn btn-danger btn-sm">Hapus</button> ';
+                return $btn;
             })
             ->rawColumns(['aksi'])
             ->make(true);
@@ -171,7 +175,7 @@ class UserController extends Controller
     }
 
     public function store_ajax(Request $request) {
-        // cek apakah request berupa ajax
+        
         if ($request->ajax() || $request->wantsJson()) {
             $rules = [
                 'level_id' => 'required|integer',
@@ -180,14 +184,14 @@ class UserController extends Controller
                 'password' => 'required|min:6'
             ];
 
-            // use Illuminate\Support\Facades\Validator;
+           
             $validator = Validator::make($request->all(), $rules);
 
             if ($validator->fails()) {
                 return response()->json([
-                    'status' => false, // response status, false: error/gagal, true: berhasil
+                    'status' => false, 
                     'message' => 'Validasi Gagal',
-                    'msgField' => $validator->errors(), // pesan error validasi
+                    'msgField' => $validator->errors(),
                 ]);
             }
 
@@ -198,5 +202,52 @@ class UserController extends Controller
             ]);
         }
         redirect('/');
+    }
+
+    public function edit_ajax(string $id) {
+        $user = UserModel::find($id);
+        $level = LevelModel::select('level_id', 'level_name')->get();
+
+        return view('user.edit_ajax', ['user' => $user, 'level' => $level]);
+    }
+
+    public function update_ajax(Request $request, $id) {
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'level_id' => 'required|integer',
+                'username' => 'required|max:20|unique:m_user,username,'.$id.',user_id',
+                'nama'     => 'required|max:100',
+                'password' => 'nullable|min:6|max:20'
+            ];
+
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false, 
+                    'message' => 'Validasi gagal.',
+                    'msgField' => $validator->errors(), 
+                ]);
+            }
+
+            $check = UserModel::find($id);
+            if ($check) {
+                if (!$request->filled('password')) { 
+                    $request->request->remove('password');
+                }
+
+                $check->update($request->all());
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil diupdate'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan'
+                ]);
+            }
+        }
+        return redirect('/');
     }
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -6,7 +6,8 @@
             <h3 class="card-title">{{ $page->title }}</h3>
             <div class="card-tools">
                 <a class="btn btn-sm btn-primary mt-1" href="{{ url('user/create') }}">Tambah</a>
-                <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah Ajax</button>
+                <button onclick="modalAction('{{ url('user/create_ajax') }}')" class="btn btn-sm btn-success mt-1">Tambah
+                    Ajax</button>
             </div>
         </div>
         <div class="card-body">
@@ -16,35 +17,37 @@
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
-
             <div class="row">
-                <label class="col-1 control-label col-form-label">Filter:</label>
-                <div class="col-3">
-                    <select class="form-control" id="level_id" name="level_id" required>
-                        <option value="">- Semua -</option>
-                        @foreach ($level as $item)
-                            <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
-                        @endforeach
-                    </select>
-                    <small class="form-text text-muted">Level Pengguna</small>
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter :</label>
+                        <div class="col-3">
+                            <select class="form-control" id="level_id" name="level_id" required>
+                                <option value="">- Semua -< /option>
+                                        @foreach ($level as $item)
+                                <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Level Pengguna</small>
+                        </div>
+                    </div>
                 </div>
             </div>
-
             <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                 <thead>
                     <tr>
                         <th>ID</th>
-                        <th>USERNAME</th>
-                        <th>NAMA</th>
-                        <th>LEVEL PENGGUNA</th>
-                        <th>AKSI</th>
+                        <th>Username</th>
+                        <th>Nama</th>
+                        <th>Level</th>
+                        <th>Aksi</th>
                     </tr>
                 </thead>
             </table>
         </div>
     </div>
-    <!-- Tambahkan Modal -->
-    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false" data-width="75%" aria-hidden="true"></div>
+    <div id="myModal" class="modal fade animate shake" tabindex="-1" role="dialog" data-backdrop="static"
+        data-keyboard="false" data-width="75%" aria-hidden="true"></div>
 @endsection
 
 @push('css')
@@ -52,62 +55,57 @@
 
 @push('js')
     <script>
+        
         function modalAction(url = '') {
-            $('#myModal').load(url, function () {
+            $('#myModal').load(url, function() {
                 $('#myModal').modal('show');
             });
         }
+        
+        var dataUser;
         $(document).ready(function() {
-            var dataUser = $('#table_user').DataTable({
+                dataUser = $('#table_user').DataTable({
                 serverSide: true,
                 ajax: {
-                    "url": "{{ url('user/list') }}",
-                    'dataType': 'json',
-                    'type': 'POST',
-                    'headers': {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    },
-                    "data": function(d) {
+                    'url': "{{ url('user/list') }}",
+                    'dataType': "json",
+                    'type': "POST",
+                    'data': function(d) {
                         d.level_id = $('#level_id').val();
                     }
                 },
-                columns: [
-                    {
-                        data: "DT_RowIndex",
-                        className: 'text-center',
-                        orderable: false,
-                        searchable: false,
-                    },
-                    {
-                        data: 'username',
-                        className: "",
-                        orderable: true,
-                        searchable: true,
-                    },
-                    {
-                        data: 'nama',
-                        className: "",
-                        orderable: true,
-                        searchable: true,
-                    },
-                    {
-                        data: 'level.level_nama',
-                        className: "",
-                        orderable: false,
-                        searchable: false,
-                    },
-                    {
-                        data: 'aksi',
-                        className: "text-center",
-                        orderable: false,
-                        searchable: false,
-                    }
-                ]
+                columns: [{
+                    data: 'DT_RowIndex',
+                    className: 'text-center',
+                    orderable: false,
+                    searchable: false
+                }, {
+                    data: "username",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                }, {
+                    data: "nama",
+                    className: "",
+                    orderable: true,
+                    searchable: true
+                }, {
+                    data: "level.level_nama",
+                    className: "",
+                    orderable: false,
+                    searchable: false
+                }, {
+                    data: "aksi",
+                    className: "",
+                    orderable: false,
+                    searchable: false
+                }]
             });
 
             $('#level_id').on('change', function() {
                 dataUser.ajax.reload();
             });
+
         });
     </script>
 @endpush
