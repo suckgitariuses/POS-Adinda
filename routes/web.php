fe186666179157e82d965929b67439ce01b19360
Adinda Mirza Devani <143467871+dndvni@users.noreply.github.com>
2025-05-10 20:38:52 +0700

    JB 8 : Implementasi Export PDF di Laravel dengan DomPDF

--- routes/web.php
+++ routes/web.php
@@ -138,6 +138,7 @@ Route::middleware(['auth'])->group(function() { // artinya semua route di dalam 
             Route::get('/import', [BarangController::class, 'import']); //ajax form upload excel
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
             Route::get('/export_excel', [BarangController::class, 'export_excel']);
+            Route::get('/export_pdf', [BarangController::class, 'export_pdf']); // export pdf
         });
     });
 });
